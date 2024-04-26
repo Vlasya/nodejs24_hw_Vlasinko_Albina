9026cbe263be2bee3c96fc145e8f62887eb4a240
Альбина <albina@My-Mac.local>
2024-04-26 12:39:12 +0300

    added env and update logger fn

---
+++ constants.js
@@ -0,0 +1,17 @@
+const COLORS_ENABLED_TYPES = {
+  ACTIVE: 1,
+  INACTIVE: 0,
+};
+
+const LOG_LEVEL_TYPES = {
+  INFO: 'info',
+  WARN: 'warn',
+  ERROR: 'error',
+};
+
+const constants = {
+  COLORS_TYPES: COLORS_ENABLED_TYPES,
+  LEVEL_TYPES: LOG_LEVEL_TYPES,
+};
+
+module.exports = constants;
--- utils/logger.js
+++ utils/logger.js
@@ -1,8 +1,28 @@
+const { bgGreen, bgYellow, bgRed } = require('colors/safe');
+const { LEVEL_TYPES } = require('../constants');
+const checkColor = require('../helpers/checkLogColors');
+const config = require('config');
+
 function logger(moduleName) {
+  const colorIsActive = !!+config.colorIsEnable;
+
+  const warn = config.logLevel === LEVEL_TYPES.WARN;
+  const info = config.logLevel === LEVEL_TYPES.INFO;
+
   return {
-    info: (...args) => console.log(`${moduleName}:`, ...args),
-    warn: (...args) => console.warn(`${moduleName}:`, ...args),
-    error: (...args) => console.error(`${moduleName}:`, ...args),
+    info: (...args) => {
+      if (info) {
+        console.log(checkColor(colorIsActive, bgGreen, moduleName), ...args);
+      }
+    },
+    warn: (...args) => {
+      if (warn || info) {
+        console.warn(checkColor(colorIsActive, bgYellow, moduleName), ...args);
+      }
+    },
+    error: (...args) => {
+      console.error(checkColor(colorIsActive, bgRed, moduleName), ...args);
+    },
   };
 }
 
